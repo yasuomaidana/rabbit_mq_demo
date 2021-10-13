bcd5601a7f625b022ad2d3f7f041da6f472f0891
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-13 14:48:00 -0500

    Runs the containers

---
+++ runall.py
@@ -0,0 +1,11 @@
+import os
+def searchContainerbyName(containerName):
+    a = os.popen('docker ps -all --filter "name={}"'.format(containerName)).readlines()
+    a.pop(0)
+    return len(a)>0
+#Runs rabbitmodule
+if (searchContainerbyName("rabbitmodule")):
+    os.system('docker start rabbitmodule')
+else:
+    os.system('docker run -d --hostname my-rabbit --name rabbitmodule -p 8085:15672 rabbitmq:3-management')
+
