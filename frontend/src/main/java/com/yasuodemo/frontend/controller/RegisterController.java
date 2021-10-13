cc7aa860764db5e36cb424c4ec2f419fbeac1ab4
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-13 12:50:28 -0500

    reciving data from registerview

--- frontend/src/main/java/com/yasuodemo/frontend/controller/RegisterController.java
+++ frontend/src/main/java/com/yasuodemo/frontend/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package com.yasuodemo.frontend.controller;
 
+import com.yasuodemo.frontend.dto.DogDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +13,10 @@ public class RegisterController {
     @GetMapping
     public String register(){
         return "register";
+    }
+    @PostMapping
+    public String newDogRegister(Model model, DogDto dog){
+        System.out.println(dog.getName());
+        return "show";
     }
 }
---
+++ frontend/src/main/java/com/yasuodemo/frontend/dto/DogDto.java
@@ -0,0 +1,12 @@
+package com.yasuodemo.frontend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class DogsDto {
+
+}
