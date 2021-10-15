d5dcba0ac9af5febd426cfc4a8ad93c07b1c0c75
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-15 13:53:08 -0500

    backend added to maven project

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/controller/DogListener.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/controller/DogListener.java
@@ -1,7 +1,7 @@
 package com.yasuodemo.backend.controller;
 
 import com.yasuodemo.backend.config.MQConfig;
-import com.yasuodemo.backend.dto.DogDto;
+import dto.DogDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
