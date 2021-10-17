eaa75eb585231bc2bee0990dbce279d18f2b607f
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-17 17:53:57 -0500

    Dog entity created

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/entity/DogEntity.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/entity/DogEntity.java
@@ -1,2 +1,21 @@
-package com.yasuodemo.backend.entity;public class DogEntity {
+package com.yasuodemo.backend.entity;
+
+import dto.DogDto;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Dogs")
+@Data
+public class DogEntity{
+    @Id
+    @Column(name="iddogs")
+    private int id;
+    private String name;
+    private String race;
+    private byte age;
 }
