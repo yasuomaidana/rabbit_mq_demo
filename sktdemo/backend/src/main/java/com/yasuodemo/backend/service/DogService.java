5a3b246619e3e58cb54e8071379ef5c9fae120b2
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-17 17:55:57 -0500

    backend dogservice implemented, it saves and returns dogs as list

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/service/DogService.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/service/DogService.java
@@ -1,2 +1,33 @@
-package com.yasuodemo.backend.service;public class DogService {
+package com.yasuodemo.backend.service;
+
+import com.yasuodemo.backend.repository.DogRepository;
+import dto.DogDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class DogService {
+    private DogRepository repository;
+
+    public List<DogDto> getDogs(){
+        return repository.getDogs().stream()
+                .map(dogEntity ->
+                        {
+                            return DogDto.builder()
+                                    .name(dogEntity.getName())
+                                    .race(dogEntity.getRace())
+                                    .age(dogEntity.getAge())
+                                    .build();
+                        }
+                )
+                .collect(Collectors.toList());
+    }
+
+    public void saveDog(DogDto dog){
+        repository.saveDog(dog.getName(), dog.getRace(),dog.getAge());
+    }
 }
