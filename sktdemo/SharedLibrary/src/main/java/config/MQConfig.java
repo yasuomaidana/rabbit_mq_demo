8791088cc8305edcfb3633ad9ad70449c149d0bf
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-17 17:45:48 -0500

    new queues added

--- sktdemo/SharedLibrary/src/main/java/config/MQConfig.java
+++ sktdemo/SharedLibrary/src/main/java/config/MQConfig.java
@@ -8,25 +8,45 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MQConfig {
-    public static final String QUEUE = "message_queue";
-    public static final String EXCHANGE = "message_exchange";
-    public static final String ROUTING_KEY = "message_routingKey";
-
-    @Bean
-    public Queue queue(){
-        return new Queue(QUEUE);
-    }
+    public static final String EXCHANGE = "skt_demo_exchange";
+    public static final String SAVE_QUEUE = "save_queue";
+    public static final String SHOW_QUEUE="show_queue";
+    public static final String SHOW_ANSWER_QUEUE="show_answer_queue";
+    public static final String SAVE_ROUTING_KEY = "save_routingKey";
+    public static final String SHOW_ROUTING_KEY = "show_routingKey";
+    public static final String SHOW_ANSWER_ROUTING_KEY = "show_answer_routingKey";
 
     @Bean
     public TopicExchange exchange(){
         return new TopicExchange(EXCHANGE);
     }
-
+    //Queues
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange){
+    public Queue saveQueue(){
+        return new Queue(SAVE_QUEUE);
+    }
+    @Bean
+    public Queue showQueue(){return new Queue(SHOW_QUEUE);}
+    @Bean
+    public Queue showAnswerQueue(){return new Queue(SHOW_ANSWER_QUEUE);}
+    //Binding
+    @Bean
+    public Binding saveBinding(Queue saveQueue, TopicExchange exchange){
         return BindingBuilder
-                .bind(queue)
-                .to(exchange).with(ROUTING_KEY);
+                .bind(saveQueue)
+                .to(exchange).with(SAVE_ROUTING_KEY);
+    }
+    @Bean
+    public Binding showBinding(Queue showQueue, TopicExchange exchange){
+        return BindingBuilder
+                .bind(showQueue)
+                .to(exchange).with(SHOW_ROUTING_KEY);
+    }
+    @Bean
+    public Binding showAnswerBinding(Queue showAnswerQueue, TopicExchange exchange){
+        return BindingBuilder
+                .bind(showAnswerQueue)
+                .to(exchange).with(SHOW_ANSWER_ROUTING_KEY);
     }
 
     @Bean
