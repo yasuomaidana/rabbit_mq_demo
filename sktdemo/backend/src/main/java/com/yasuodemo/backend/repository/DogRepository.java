93f651de8d35074c7023c1e6bc17580a44b56cc1
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-17 17:55:20 -0500

    dog repository created, it supports two stored procedures

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/repository/DogRepository.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/repository/DogRepository.java
@@ -1,2 +1,26 @@
-package com.yasuodemo.backend.repository;public class DogRepository {
+package com.yasuodemo.backend.repository;
+
+import com.yasuodemo.backend.entity.DogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface DogRepository extends JpaRepository<DogEntity,Integer> {
+
+    @Query(value ="{call getDogs()}",nativeQuery = true)
+    List<DogEntity> getDogs();
+
+    @Transactional
+    @Modifying
+    @Query(value="{call saveDog(:nameIn, :raceIn, :ageIN)}",nativeQuery = true)
+    void saveDog(
+            @Param("nameIn") String nameIn,
+            @Param("raceIn") String raceIn,
+            @Param("ageIN") Byte ageIn);
 }
