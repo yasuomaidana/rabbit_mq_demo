4c17d803ede58842e660d7c37278ac3b11a27ef0
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-17 17:47:53 -0500

    listeners configured, show listener implemented

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/controller/DogListener.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/controller/DogListener.java
@@ -1,15 +1,28 @@
 package com.yasuodemo.backend.controller;
 
 import com.yasuodemo.backend.config.MQConfig;
+import com.yasuodemo.backend.service.DogService;
 import dto.DogDto;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.ReceiveAndReplyCallback;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class DogListener {
 
-    @RabbitListener(queues = MQConfig.QUEUE)
+    private RabbitTemplate template;
+    private DogService dogService;
+
+    @RabbitListener(queues = MQConfig.SAVE_QUEUE)
     public void listenNewDog(DogDto dog){
-         System.out.println("Preparing to save : "+dog);
+        System.out.println("Preparing to save : "+dog);
+    }
+
+    @RabbitListener(queues = MQConfig.SHOW_QUEUE)
+    public void listenShowDog(String message){
+        template.convertAndSend(MQConfig.EXCHANGE,MQConfig.SHOW_ANSWER_ROUTING_KEY,dogService.getDogs());
     }
 }
