75f4096e40793e946c09ee572eaddc7235eeb215
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-18 16:33:03 -0500

    creating new listeners

--- sktdemo/backend/src/main/java/com/yasuodemo/backend/config/MQConfig.java
+++ sktdemo/backend/src/main/java/com/yasuodemo/backend/config/MQConfig.java
@@ -1,12 +1,14 @@
 package com.yasuodemo.backend.config;
 
 import com.yasuodemo.backend.controller.dogListener.SaveListener;
+import com.yasuodemo.backend.controller.dogListener.ShowListener;
 import com.yasuodemo.backend.service.DogService;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,16 +22,24 @@ public class MQConfig extends config.MQConfig {
         return template;
     }
 
-
-
     @Bean
-    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory,DogService dogService, RabbitTemplate rabbitTemplate) {
-        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-        simpleMessageListenerContainer.setQueueNames(rabbitConstants().saveQueue);
-        simpleMessageListenerContainer.setMessageListener(new SaveListener(dogService,rabbitTemplate));
-        return simpleMessageListenerContainer;
-
+    public MessageListenerContainer saveListenerContainer(ConnectionFactory connectionFactory,DogService dogService, RabbitTemplate rabbitTemplate) {
+        SimpleMessageListenerContainer container = containerGenerator(connectionFactory,rabbitConstants().saveQueue);
+        container.setMessageListener(new SaveListener(dogService,rabbitTemplate));
+        return container;
     }
 
+    @Bean @Autowired
+    public MessageListenerContainer showListenerContainer(ConnectionFactory connectionFactory,DogService dogService, RabbitTemplate rabbitTemplate) {
+        SimpleMessageListenerContainer container = containerGenerator(connectionFactory,rabbitConstants().showQueue);
+        container.setMessageListener(new ShowListener(dogService,rabbitTemplate,rabbitConstants()));
+        return container;
+    }
+
+    private SimpleMessageListenerContainer containerGenerator(ConnectionFactory connectionFactory,String queueName){
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+        simpleMessageListenerContainer.setQueueNames(queueName);
+        return simpleMessageListenerContainer;
+    }
 }
