a51ecba1e0f4536c309b6146b55c16dc5266f612
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2021-10-19 12:31:21 -0500

    Starting to develop error handlers

--- sktdemo/frontend/src/main/java/com/example/frontend/controller/DogController.java
+++ sktdemo/frontend/src/main/java/com/example/frontend/controller/DogController.java
@@ -5,6 +5,7 @@ import dto.DogDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,10 @@ public class DogController {
     }
 
     @PostMapping("/register")
-    public String newDogRegister(Model model,DogDto dog){
+    public String newDogRegister(Model model, DogDto dog, Errors errors){
+        if(errors.hasErrors()){
+            System.out.println("ENTER");
+        }
         dogService.saveDog(dog);
         return "redirect:/show";
     }
